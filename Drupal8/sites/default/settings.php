04d06938e814f86a7f0ce1d8e0999778bbbc2378
Michael Schmid <michael@amazeelabs.com>
2016-09-15 16:46:48 -0500

    solr connection

--- Drupal8/sites/default/settings.php
+++ Drupal8/sites/default/settings.php
@@ -38,6 +38,20 @@ if(getenv('AMAZEEIO_SITENAME')){
   );
 }
 
+### amazee.io Solr connection
+// WARNING: you have to create a search_api server having "solr" machine name at
+// /admin/config/search/search-api/add-server to make this work.
+if (getenv('AMAZEEIO_SOLR_HOST') && getenv('AMAZEEIO_SOLR_PORT')) {
+  $config['search_api.server.solr']['backend_config']['host'] = getenv('AMAZEEIO_SOLR_HOST');
+  $config['search_api.server.solr']['backend_config']['path'] = '/solr/' . (getenv('AMAZEEIO_SOLR_CORE') ?: getenv('AMAZEEIO_SITENAME')) . '/';
+  $config['search_api.server.solr']['backend_config']['port'] = getenv('AMAZEEIO_SOLR_PORT');
+  $config['search_api.server.solr']['backend_config']['http_user'] = (getenv('AMAZEEIO_SOLR_USER') ?: '');
+  $config['search_api.server.solr']['backend_config']['http']['http_user'] = (getenv('AMAZEEIO_SOLR_USER') ?: '');
+  $config['search_api.server.solr']['backend_config']['http_pass'] = (getenv('AMAZEEIO_SOLR_PASSWORD') ?: '');
+  $config['search_api.server.solr']['backend_config']['http']['http_pass'] = (getenv('AMAZEEIO_SOLR_PASSWORD') ?: '');
+  $config['search_api.server.solr']['name'] = 'AmazeeIO Solr - Environment: ' . getenv('AMAZEEIO_SITE_ENVIRONMENT');
+}
+
 ### amazee.io Varnish & Reverse proxy settings
 if (getenv('AMAZEEIO_VARNISH_HOSTS') && getenv('AMAZEEIO_VARNISH_SECRET')) {
   $varnish_hosts = explode(getenv('AMAZEEIO_VARNISH_HOSTS'), ',');
