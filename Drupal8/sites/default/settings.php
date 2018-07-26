bc55831662b3560df2f5c2b724e6fad82c93adfd
Michael Schmid <michael@amazee.com>
2018-07-26 08:38:58 -0500

    amazee.io D8 Redis Config

--- Drupal8/sites/default/settings.php
+++ Drupal8/sites/default/settings.php
@@ -66,6 +66,56 @@ if (getenv('AMAZEEIO_VARNISH_HOSTS') && getenv('AMAZEEIO_VARNISH_SECRET')) {
   $config['varnish.settings']['varnish_version'] = 4;
 }
 
+### amazee.io Redis connection if Drupal Redis module exists and PHP Redis Module is loaded
+if (getenv('AMAZEEIO_REDIS_HOST') && file_exists($app_root . '/modules/contrib/redis') && extension_loaded('redis')){
+  $settings['redis.connection']['interface'] = 'PhpRedis';
+  $settings['redis.connection']['host'] = getenv('AMAZEEIO_REDIS_HOST');
+  $settings['redis.connection']['port'] = getenv('AMAZEEIO_REDIS_PORT');
+  $settings['cache_prefix']['default'] = getenv('AMAZEEIO_SITENAME');
+  # Do not set the cache during installations of Drupal
+  if (!drupal_installation_attempted()) {
+    $settings['cache']['default'] = 'cache.backend.redis';
+    // Include the default example.services.yml from the module, which will
+    // replace all supported backend services (that currently includes the cache tags
+    // checksum service and the lock backends, check the file for the current list)
+    $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+    // Allow the services to work before the Redis module itself is enabled.
+    $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+    // Manually add the classloader path, this is required for the container cache bin definition below
+    // and allows to use it without the redis module being enabled.
+    $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+    // Use redis for container cache.
+    // The container cache is used to load the container definition itself, and
+    // thus any configuration stored in the container itself is not available
+    // yet. These lines force the container cache to use Redis rather than the
+    // default SQL cache.
+    $settings['bootstrap_container_definition'] = [
+      'parameters' => [],
+      'services' => [
+        'redis.factory' => [
+          'class' => 'Drupal\redis\ClientFactory',
+        ],
+        'cache.backend.redis' => [
+          'class' => 'Drupal\redis\Cache\CacheBackendFactory',
+          'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
+        ],
+        'cache.container' => [
+          'class' => '\Drupal\redis\Cache\PhpRedis',
+          'factory' => ['@cache.backend.redis', 'get'],
+          'arguments' => ['container'],
+        ],
+        'cache_tags_provider.container' => [
+          'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+          'arguments' => ['@redis.factory'],
+        ],
+        'serialization.phpserialize' => [
+          'class' => 'Drupal\Component\Serialization\PhpSerialize',
+        ],
+      ],
+    ];
+  }
+}
+
 ### Trusted Host Patterns, see https://www.drupal.org/node/2410395 for more information.
 ### If your site runs on multiple domains, you need to add these domains here
 $settings['trusted_host_patterns'] = array(
